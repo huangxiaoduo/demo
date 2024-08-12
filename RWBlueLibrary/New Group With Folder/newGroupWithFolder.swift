aa5bca6adb499299269eb485fbeb446463cdacb5
HuangZe <ze.huang@changhong.com>
2024-08-12 11:29:10 +0800

    创建文件夹

---
+++ RWBlueLibrary/New Group With Folder/newGroupWithFolder.swift
@@ -0,0 +1,13 @@
+//
+//  newGroupWithFolder.swift
+//  RWBlueLibrary
+//
+//  Created by HuangZe on 2024/8/12.
+//  Copyright © 2024 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class newGroupWithFolder: NSObject {
+
+}
---
+++ RWBlueLibrary/newGroup.swift
@@ -0,0 +1,13 @@
+//
+//  newGroup.swift
+//  RWBlueLibrary
+//
+//  Created by HuangZe on 2024/8/12.
+//  Copyright © 2024 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class newGroup: NSObject {
+
+}
